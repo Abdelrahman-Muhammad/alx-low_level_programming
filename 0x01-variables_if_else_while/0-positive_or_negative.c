264444eac4c10d6e4c0393fd5739f864124686f3
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2023-03-18 01:19:01 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,11 @@
 
 /**
  * main - Entry point
- * Description: Assigns a random number to n and prints if it's positive or negative
+ *
+ * Description: Assigns a random number to n and prints whether it's positive or negative
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -16,11 +17,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
 	else
+	{
 		printf("%d is zero\n", n);
+	}
 
 	return (0);
 }
